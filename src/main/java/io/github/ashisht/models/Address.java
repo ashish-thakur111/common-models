29749018e2b0cb782f559e36826d6100b6692ffa
ashish-thakur111 <ashish.thakur11140@gmail.com>
2021-12-26 00:50:01 +0530

    refactor

--- src/main/java/io/github/ashisht/models/Address.java
+++ src/main/java/io/github/ashisht/models/Address.java
@@ -1,13 +1,17 @@
 package io.github.ashisht.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author ashish.thakur on 12-10-2021
  */
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     private String street;
     private String city;
--- src/main/java/io/github/ashisht/models/Person.java
+++ src/main/java/io/github/ashisht/models/Person.java
@@ -1,15 +1,20 @@
 package io.github.ashisht.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author ashish.thakur on 12-10-2021
  */
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private String name;
     private Integer age;
+    private String maritalStatus;
     private Address address;
 }
